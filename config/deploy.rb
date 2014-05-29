9e16ddd5d209333ce10955bcfbec86bbcede5759
sriman-darbha <sriman1988@gmail.com>
2014-05-29 14:36:20 +0530

    changed values in deploy.rb deploy folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,38 +1,35 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'testapp1'
-set :user, "sriman"
-set :scm_passphrase, "sriman"
-set :repo_url, 'https://github.com/sriman-darbha/testapp1'
-set :git_shallow_clone, 1
+set :application, 'my_app_name'
+set :repo_url, 'git@example.com:me/my_repo.git'
+
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
- set :deploy_to, '/var/www/sample'
+# set :deploy_to, '/var/www/my_app'
 
- set :ssh_options, { :forward_agent => true }
 # Default value for :scm is :git
- set :scm, :git
+# set :scm, :git
 
 # Default value for :format is :pretty
- set :format, :pretty
+# set :format, :pretty
 
 # Default value for :log_level is :debug
 # set :log_level, :debug
 
 # Default value for :pty is false
- set :pty, true
+# set :pty, true
 
 # Default value for :linked_files is []
- set :linked_files, %w{config/database.yml}
+# set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
- set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
- set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
@@ -43,7 +40,7 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-       execute :touch, release_path.join('tmp/restart.txt')
+      # execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
@@ -52,9 +49,9 @@ namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
-       within release_path do
-         execute :rake, 'cache:clear'
-       end
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
     end
   end
 
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{test@example.com}
-role :web, %w{test@example.com}
-role :db,  %w{test@example.com}
+role :app, %w{deploy@example.com}
+role :web, %w{deploy@example.com}
+role :db,  %w{deploy@example.com}
 
 # Extended Server Syntax
 # ======================
@@ -14,7 +14,7 @@ role :db,  %w{test@example.com}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'example.com', user: 'sriman', roles: %w{web app}, my_property: :my_value
+server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
@@ -37,7 +37,3 @@ server 'example.com', user: 'sriman', roles: %w{web app}, my_property: :my_value
 #     # password: 'please use keys'
 #   }
 # setting per server overrides global ssh_options
- Net::SSH.start("host", "user", :password => "password") do |ssh|
-   result = ssh.exec!("ls -l")
-   puts result
- end
